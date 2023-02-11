e8a310e0531c9bc2828ebc7bfae11b12b76ec5d4
jimoh yusuf <expensive7832@gmail.com>
2023-02-11 17:58:23 +0100

    user account created

--- account/models.py
+++ account/models.py
@@ -3,13 +3,11 @@ from django.contrib.auth.models import AbstractUser
 from .manager import CustomUserManager
 # Create your models here.
 class User(AbstractUser):
-    email = models.CharField(max_length=50)
-    password = models.CharField(max_length=255)
-    username = models.CharField(max_length=30)
+    email = models.CharField(max_length=50, unique=True)
     photo = models.ImageField(upload_to="profile")
 
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = [password]
+    REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
