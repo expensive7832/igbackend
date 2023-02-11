33389a5bd6d26ba10f066dde746968e18f63e2cf
jimoh yusuf <expensive7832@gmail.com>
2023-02-11 14:42:23 +0100

    users account done

--- account/models.py
+++ account/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from .manager import CustomUserManager
 # Create your models here.
+class User(AbstractUser):
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=255)
+    username = models.CharField(max_length=30)
+    photo = models.ImageField(upload_to="profile")
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [password]
+
+    objects = CustomUserManager()
--- igbackend/settings.py
+++ igbackend/settings.py
@@ -87,6 +87,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'account.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
